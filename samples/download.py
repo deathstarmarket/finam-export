a754d6a2234563825caa7417b19be2ca18363629
deathstarmarket <ivan.legenchuk@specialsituations.net>
2021-10-07 00:29:30 +0300

    watch_list calculation corrected, local data import added

--- samples/download.py
+++ samples/download.py
@@ -31,13 +31,32 @@ def main():
 
     codes=pd.read_csv("codes.csv", delimiter = "\t", index_col="id")
     #print(codes)
+    codes=pd.DataFrame(codes.tail(255))
 
     averages=[]
     drop_list=[]
+    prices=[]
 
     for code in codes["code"]:
         try:
+            data=pd.read_csv(f"{code}"+".csv", sep="\t", encoding="utf-8")
+            data=pd.DataFrame(data.tail(255))
 
+            yearly_average=round(np.average(data["<CLOSE>"], weights=data["<VOL>"]),2)
+            averages.append(yearly_average)
+
+            tail=pd.DataFrame(data.tail(1))
+            price=int(tail.loc[:]["<CLOSE>"])
+            prices.append(price)
+
+            drop=round(price/yearly_average-1,2)
+            drop_list.append(drop)
+
+            print(code, "Yearly_average", yearly_average, "Price", price, "Drop", drop)
+
+            #print(data)
+
+            """downloading script
             print("Downloading", code)
             data = exporter.lookup(code=code, market=Market.SHARES,code_comparator=LookupComparator.EQUALS)
 
@@ -53,17 +72,30 @@ def main():
             price=int(tail.loc[:]["<CLOSE>"])
             drop=round(price/yearly_average-1,2)
             drop_list.append(drop)
-            print("Yearly_average", yearly_average, "Price", price, "Drop", drop)
+            print(code, "Yearly_average", yearly_average, "Price", price, "Drop", drop)
+            """
 
 
         except ZeroDivisionError:
             print("ZeroDivisionError")
+            yearly_average=0
+            drop=0
+            price=0
+            prices.append(price)
+            drop_list.append(drop)
+            averages.append(yearly_average)
 
-    codes["averages"]=averages
-    codes["drop"]=drop_list
-    watch_list.DataFrame=codes[["drop"] < -0.1]
-    watch_list.to_excel("watch_list.xls")
-    codes.to_excel("codes.xls")
+
+    codes["VWAP255"]=averages
+    codes["PRICE"]=prices
+    codes["DROP"]=drop_list
+    print (codes)
+    watch_list=codes[codes["DROP"] < -0.1]
+    watch_list=watch_list[watch_list["DROP"] > -1]
+    watch_list=watch_list.sort_values(by="DROP")
+    watch_list.to_csv("watch_list.csv")
+    codes.to_csv("codes_results.csv")
+
 
 
 if __name__ == '__main__':
